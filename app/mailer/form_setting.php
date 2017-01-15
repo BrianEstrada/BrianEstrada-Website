f98927c9aeed3d3254841a14f4bc56e9619a2f9b
Brian Estrada <lovesguitar@gmail.com>
2017-01-14 20:14:42 -0800

    Updated a few other things

--- app/mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from the site 'You Site Name'"; //letter subject
-	$to = 'contact@brianestrada.com'; // Enter Your E-mail
-	$from = 'mail@brianestrada.com'; // Admin e-mail
-	$fromName = 'Brian Estrada'; // Your company name
-	$charset = 'UTF-8';
-?>
